5d96d227dddb9961f43592b4f924a9f2d11b0141
ashfinenayet <ashfin.enayet@gmail.com>
2022-03-13 16:32:29 -0400

    fixed automation

--- spammessage.py
+++ spammessage.py
@@ -3,4 +3,9 @@ import pyautogui
 
 def SendMessage():
     time.sleep(4)
-    text = open('message.txt')
+    text = open('message1.txt')
+    for each_line in text:
+        pyautogui.typewrite(each_line)
+        pyautogui.press('enter')
+
+SendMessage()
